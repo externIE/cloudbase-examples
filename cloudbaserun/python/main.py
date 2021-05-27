a69fbaeacac611f19274dde89f01b32dc36607e3
林义凯 <externIE@outlook.com>
2021-05-27 20:14:27 +0800

    Update main.py

--- cloudbaserun/python/main.py
+++ cloudbaserun/python/main.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 
 from flask import Flask
 
@@ -6,7 +7,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    now = datetime.datetime.now()
+    print("北京时间 %s" % now.strftime("%Y-%m-%d %H:%M:%S"))
+    delta = datetime.timedelta(hours=-12)
+    print("纽约时间 %s" % (now+delta).strftime("%Y-%m-%d %H:%M:%S"))
+
+    delta = datetime.timedelta(hours=-7)
+    print("伦敦时间 %s" % (now+delta).strftime("%Y-%m-%d %H:%M:%S"))
+
+    delta = datetime.timedelta(hours=1)
+    print("大阪时间 %s" % (now+delta).strftime("%Y-%m-%d %H:%M:%S"))
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8080)
