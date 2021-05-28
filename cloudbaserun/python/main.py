5bca31ddc76b71ac02030c4b3759ba34fb48a26f
林义凯 <externIE@outlook.com>
2021-05-28 10:21:32 +0800

    Update main.py

--- cloudbaserun/python/main.py
+++ cloudbaserun/python/main.py
@@ -8,15 +8,14 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     now = datetime.datetime.now()
-    print("北京时间 %s" % now.strftime("%Y-%m-%d %H:%M:%S"))
+    s = "北京时间 %s\n" % now.strftime("%Y-%m-%d %H:%M:%S")
     delta = datetime.timedelta(hours=-12)
-    print("纽约时间 %s" % (now+delta).strftime("%Y-%m-%d %H:%M:%S"))
-
+    s += "纽约时间 %s\n" % (now+delta).strftime("%Y-%m-%d %H:%M:%S")
     delta = datetime.timedelta(hours=-7)
-    print("伦敦时间 %s" % (now+delta).strftime("%Y-%m-%d %H:%M:%S"))
-
+    s += "伦敦时间 %s\n" % (now+delta).strftime("%Y-%m-%d %H:%M:%S")
     delta = datetime.timedelta(hours=1)
-    print("大阪时间 %s" % (now+delta).strftime("%Y-%m-%d %H:%M:%S"))
+    s += "大阪时间 %s\n" % (now+delta).strftime("%Y-%m-%d %H:%M:%S")
+    return s
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8080)
